1ff249f54cfa8a7d1fb84ff5e96ce9bd838eb98b
fangkai <1209683835@qq.com>
2022-06-16 16:13:37 +0800

    first commit

---
+++ src/components/Header/index.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+    <van-nav-bar :title="title" left-text="返回" left-arrow @click-left="goBack" v-show="showHeader"/>   
+</div>
+     
+</template>
+<script>
+import { NavBar } from 'vant';
+
+export default {
+    name:"v-header",
+    data(){
+        return {
+            title:"",
+        };
+    },
+
+    created() {
+        this.title = this.$store.getters.titleHeader;
+    },
+    computed: {
+        showHeader:function(){
+            let header = this.$store.getters.showHeader;
+            return header
+        }
+    },
+    methods: {
+        goBack() {
+            this.$router.back(-1);
+        }
+   },
+   components: {
+    [NavBar.name]:NavBar,
+  }
+}
+</script>
